88f8bfa4c176a2c3daecfeee97086719372accd8
bowook <mbosx316@naver.com>
2025-04-19 18:50:21 +0900

    test(MissionOneStepTest) : 테스트 코드 직관적이게 분리

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -14,29 +15,34 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
+    @DisplayName("localhost:8080/admin 요청 시 상태 값 반환 확인")
     @Test
-    void 일단계() {
+    void test1() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
     }
 
+    @DisplayName("localhost:8080/admin/reservation 요청 시 상태 값 반환 확인")
     @Test
-    void 이단계() {
+    void test2() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
     }
 
+    @DisplayName("예약 생성 테스트")
     @Test
-    void 삼단계() {
+    void test3() {
+        //given
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
         params.put("time", "15:40");
 
+        //when & then
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
@@ -44,18 +50,51 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("id", is(1));
+    }
 
+    @DisplayName("예약 목록 조회 테스트")
+    @Test
+    void test4() {
+        //given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .post("/reservations");
+
+        //when & then
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+    }
 
+    @DisplayName("예약 삭제 테스트")
+    @Test
+    void test5() {
+        //given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .post("/reservations");
+
+        //when
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(200);
 
+        //then
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
