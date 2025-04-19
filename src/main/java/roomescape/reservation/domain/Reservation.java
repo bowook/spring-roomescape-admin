45fc85c23afbaa6680d3c449fe4f84351c12571e
bowook <mbosx316@naver.com>
2025-04-19 18:44:37 +0900

    refactor(Reservation) : DTO 추가에 따른 JsonFormat 어노테이션 삭제

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -1,15 +1,13 @@
 package roomescape.reservation.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import roomescape.reservation.dto.ReservationRequest;
 
 public class Reservation {
     private final Long id;
     private final String name;
-    @JsonFormat(pattern = "yyyy-MM-dd")
     private final LocalDate date;
-    @JsonFormat(pattern = "HH:mm")
     private final LocalTime time;
 
     public Reservation(final long id, final String name, final LocalDate date, final LocalTime time) {
@@ -19,8 +17,8 @@ public class Reservation {
         this.time = time;
     }
 
-    public static Reservation toEntity(Reservation reservation, Long id) {
-        return new Reservation(id, reservation.name, reservation.date, reservation.time);
+    public static Reservation withId(ReservationRequest reservationRequest, Long id) {
+        return new Reservation(id, reservationRequest.name(), reservationRequest.date(), reservationRequest.time());
     }
 
     public boolean isSameId(final long id) {
@@ -33,5 +31,13 @@ public class Reservation {
 
     public String getName() {
         return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
     }
 }
